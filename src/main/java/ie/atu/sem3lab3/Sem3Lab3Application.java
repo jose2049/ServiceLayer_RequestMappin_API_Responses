6e7615e27566bc8b1f9f115eec607c2024c66449
Jose Alvarez <g00463862@atu.ie>
2026-02-11 16:00:47 +0000

    first commit

---
+++ src/main/java/ie/atu/sem3lab3/Sem3Lab3Application.java
@@ -0,0 +1,14 @@
+package ie.atu.sem3lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+Sem3Lab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem3Lab3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/sem3lab3/Sem3Lab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.sem3lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem3Lab3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
